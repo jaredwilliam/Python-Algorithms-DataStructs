3a057763dd8cf6962567c4aa0400fc94fb6fba18
Jared <jared.w.early@gmail.com>
2021-01-24 19:11:55 -0500

    Adding unit testing notes

---
+++ 00 Python/Errors and Exception Handling/cap.py
@@ -0,0 +1,9 @@
+def cap_text(text):
+    '''
+    Input a string
+    Output the capitalized string
+    '''
+    # capitalize() only impacts the first letter of a string
+    # we can use title() to impact all individual words of a string
+#     return text.capitalize()
+    return text.title()
